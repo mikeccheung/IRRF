37bafc888774369c2c0a109b977c7cdb3b8d1e98
Michael Cheung <65215506+mikeccheung@users.noreply.github.com>
2022-04-03 16:12:58 -0700

    Small update
    - added recipeList
    
    To-do:
    - fix acceptReject loop to show a different recipe instead of starting at the beginning again
    - add accepted recipes to recipeList
    - make it look nicer
    - update readme with instructions to generate your own API key from spoonacular for your config.py
    
    Future improvements:
    - split each component into individual file

--- RecipeFinder.py
+++ RecipeFinder.py
@@ -2,50 +2,57 @@ import requests
 import config
 
 
-print("Welcome to the Recipe Finder, the smart way to eat.")
-
-done = 0
-ingrList = []
-shoppingList = []
-
-ingredients = str(input("What ingredients do you have on hand?\nSeperate with commas (ie. apple, cheese, butter.)\n"))
+def getIngredients(missingIngredients):
+    ingredients = str(input("What ingredients do you have on hand?\nSeperate with commas (ie. apple, cheese, butter.)\n"))
+    parsedIngredients = (inputToList(ingredients))
+    res = getRecipeByIngredients(parsedIngredients, missingIngredients)
 
 ## change inputs to a list
 def inputToList(string):
     ingrList = list(string.split(", "))
     return ingrList
 
-print("The ingredients you've entered are:")
-print(inputToList(ingredients))
-
-answer = ""
-ans = input("Continue? (Y/N)\n")
-
-payload = 0
-results = []
 
 # Calls API to find recipe with the inputted ingredients
-def getRecipeByIngredients():
+def getRecipeByIngredients(ingredients, missingIngredients):
+    i = 0
+    acceptReject = "reject"
     parameters = {
-        'apiKey' : config.api_key,
+        'apiKey': config.api_key,
         'fillIngredients': False,
         'ingredients': ingredients,
         'limitLicense': True,
-        'number': 1,
+        'number': 15,
         'ranking': 1
-        }
+    }
     endpoint = "https://api.spoonacular.com/recipes/findByIngredients"
     r = requests.get(endpoint, params=parameters)
     results = r.json()
-    # need to log missed ingredients and append them to a shopping list
-    title = results[0]['title']
-    iMissed = results[0]['missedIngredientCount']
-    iUsed = results[0]['usedIngredientCount']
-#    print(iUsed)
-#    print(title)
-#    print(iMissed)
-    return "You can make " + title + "\n     using " + str(iUsed) + " of your ingredients\n     and you're only missing " + str(iMissed) + " other ingredients!"
-    
-res = getRecipeByIngredients()
+    for x in results[0 + i]["missedIngredients"]:
+        missingIngredients.append(x["name"])
 
-print(res)
+    title = results[0 + i]['title']
+    print("You can make " + title)
+    acceptReject = input("Accept or Reject? (Accept/Reject)\n")
+    i = i + 1
+
+    if acceptReject.lower == "accept":
+        return missingIngredients
+    else:
+        return
+
+if __name__ == '__main__':
+    done = 0
+    missingIngredients = []
+    recipeList = []
+
+    print("Welcome to the Recipe Finder, the smart way to eat.")
+
+    while done == 0:
+        getIngredients(missingIngredients);
+        theyDone = input("Are you finished with your list? (Y/N)\n")
+        if theyDone.lower() == "y":
+            done = 1
+    missingIngredients = list(dict.fromkeys(missingIngredients))
+    print("You are missing the following ingredients: " + ", ".join(missingIngredients))
+    print("You will be able to make the following recipes: " + ", ".join(recipeList))
