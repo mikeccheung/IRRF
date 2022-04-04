c272ea5a698af9a58acc9b0eede0f4da2997272f
Michael Cheung <65215506+mikeccheung@users.noreply.github.com>
2022-04-04 14:24:03 -0700

    Added comments for future changes
    Names given for the modules for when they are divided into separate files.

--- RecipeFinder.py
+++ RecipeFinder.py
@@ -1,19 +1,19 @@
 import requests
 import config
 
-
+## ingredients_module.py - input prompt for ingredients
 def getIngredients(missingIngredients):
     ingredients = str(input("What ingredients do you have on hand?\nSeperate with commas (ie. apple, cheese, butter.)\n"))
     parsedIngredients = (inputToList(ingredients))
     res = getRecipeByIngredients(parsedIngredients, missingIngredients)
 
-## change inputs to a list
+## inputToList_module.py - changes inputs to a list
 def inputToList(string):
     ingrList = list(string.split(", "))
     return ingrList
 
 
-# Calls API to find recipe with the inputted ingredients
+## getRecipeByIngredients_module.py - Calls API to find recipe with the inputted ingredients
 def getRecipeByIngredients(ingredients, missingIngredients):
     i = 0
     acceptReject = "reject"
@@ -33,14 +33,17 @@ def getRecipeByIngredients(ingredients, missingIngredients):
 
     title = results[0 + i]['title']
     print("You can make " + title)
-    acceptReject = input("Accept or Reject? (Accept/Reject)\n")
+    acceptReject = input("Would you like to add this to your Recipe List? (Y/N)\n")
     i = i + 1
 
-    if acceptReject.lower == "accept":
+    if acceptReject.lower == "y":
+        recipeList.append(x["title"])
         return missingIngredients
     else:
         return
 
+
+## main.py - calls modules, displays results
 if __name__ == '__main__':
     done = 0
     missingIngredients = []
